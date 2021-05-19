bdfb601caf26e4741be9c0aafcb1a4e97f72b74d
Samuel D. Villegas <villegsa@oregonstate.edu>
2021-05-18 22:16:47 -0700

    Retry table extension with updated syntax

--- docs/conf.py
+++ docs/conf.py
@@ -30,7 +30,8 @@ release = '0.0.0'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-  'recommonmark'
+  'recommonmark',
+  'sphinx-markdown-tables'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
